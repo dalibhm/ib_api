2dcf1978ab97334eecab0cef23abb0b9bef07fc4
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-11-02 13:05:34 +0000

    requirements scripts

--- stock_list.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /apps/ib_api/ib_python/stock_list
-
-python3 -m venv s_venv
-sudo s_venv/bin/pip install --upgrade pip setuptools
-sudo s_venv/bin/pip install -r requirements.txt
-
-s_venv/bin/python3 program.py
