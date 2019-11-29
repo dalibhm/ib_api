17b5fae0d462531abe3b9c008c3db3f5a4d596f4
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-11-29 20:48:35 +0000

    allow 5 seconds before reconnecting

--- ib_client/connection_manager.py
+++ ib_client/connection_manager.py
@@ -10,15 +10,18 @@ class ConnectionManager:
         self.port = config.getint('ib client', 'port')
         self.ib_client = ib_client
         self.last_connection_trial_time = datetime.now()
+        # self.disconnect_time = None
 
     def connect(self):
-        while (datetime.now() - self.last_connection_trial_time).total_seconds() < 5:
-            time.sleep(5)
-            continue
+        # while (datetime.now() - self.last_connection_trial_time).total_seconds() < 5\
+        #         or (datetime.now() - self.disconnect_time).total_seconds() < 5:
+        time.sleep(5)
+        # continue
         self.ib_client.connect(self.host, self.port, 0)
         self.last_connection_trial_time = datetime.now()
 
     def reconnect(self):
         self.ib_client.disconnect()
         self.ib_client.done = True
+        # self.disconnect_time = datetime.now()
         self.connect()
