c44203df35b0a88d53b02bd44c32257abc46fd1b
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-11-30 20:31:07 +0000

    another change in connection manager make sure it is connected before launching EClient.run()

--- ib_client/connection_manager.py
+++ ib_client/connection_manager.py
@@ -2,14 +2,17 @@ from configparser import ConfigParser
 from datetime import datetime, time
 from threading import Thread
 
-import ib_client
+from ibapi.client import EClient
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ConnectionManager:
-    def __init__(self, config: ConfigParser, ib_client: ib_client.IbClient):
+    def __init__(self, config: ConfigParser, ib_client):
         self.host = config.get('ib client', 'host')
         self.port = config.getint('ib client', 'port')
-        self.ib_client = ib_client
+        self.ib_client: EClient = ib_client
         # self.last_connection_trial_time = datetime.now()
         # self.disconnect_time = None
 
@@ -18,9 +21,12 @@ class ConnectionManager:
         #         or (datetime.now() - self.disconnect_time).total_seconds() < 5:
         # time.sleep(5)
         # continue
-        self.ib_client.connect(self.host, self.port, 0)
+        while not self.ib_client.isConnected():
+            self.ib_client.connect(self.host, self.port, 0)
+            logger.error('reconnecting to IB API')
         app_thread = Thread(target=self.ib_client.run)
         app_thread.start()
+        logger.error('EClient.run() started')
         # self.last_connection_trial_time = datetime.now()
 
     def reconnect(self):
