873985fd8a3ccb01df85e2eed25cfc487ae7c355
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-11-30 16:18:37 +0000

    removed setting EClient.done to False removed waiting 5 seconds on reconnection

--- ib_client/connection_manager.py
+++ ib_client/connection_manager.py
@@ -9,19 +9,19 @@ class ConnectionManager:
         self.host = config.get('ib client', 'host')
         self.port = config.getint('ib client', 'port')
         self.ib_client = ib_client
-        self.last_connection_trial_time = datetime.now()
+        # self.last_connection_trial_time = datetime.now()
         # self.disconnect_time = None
 
     def connect(self):
         # while (datetime.now() - self.last_connection_trial_time).total_seconds() < 5\
         #         or (datetime.now() - self.disconnect_time).total_seconds() < 5:
-        time.sleep(5)
+        # time.sleep(5)
         # continue
         self.ib_client.connect(self.host, self.port, 0)
-        self.last_connection_trial_time = datetime.now()
+        # self.last_connection_trial_time = datetime.now()
 
     def reconnect(self):
         self.ib_client.disconnect()
-        self.ib_client.done = True
+        # self.ib_client.done = True
         # self.disconnect_time = datetime.now()
         self.connect()
