11ca8bcd1f852e74a5d03f8e7840c90aa201e298
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-12-07 13:53:02 +0000

    catch exception inside the historical runner loop

--- runner/historical_runner.py
+++ runner/historical_runner.py
@@ -25,23 +25,27 @@ class HistoricalRunner(Thread):
         try:
             logger.debug('starting historical runner loop')
             while self.msg_queue:
-                contract = self.msg_queue.get()
-                latest_timestamp = self.historical_data_service.get_latest_timestamp(contract['symbol'], "%Y-%m-%d")
-                if latest_timestamp:
-                    start_date = latest_timestamp
-                params = {
-                    "start_date": start_date,  # "1999-01-01",
-                    "end_date": self.end_date,  # "2019-11-27",
-                    "bar_size": "1 day",
-                    "price_type": "TRADES"
-                }
-                arranged_params = HistoricalRequestTemplate(params).params
-                contract['exchange'] = 'SMART'
-                status = self.ib_client.request_historical_data(contract, arranged_params)
-                self.request_scheduler.request_added()
+                if not self.msg_queue.empty():
+                    try:
+                        contract = self.msg_queue.get()
+                        latest_timestamp = self.historical_data_service.get_latest_timestamp(contract['symbol'], "%Y-%m-%d")
+                        if latest_timestamp:
+                            start_date = latest_timestamp
+                        params = {
+                            "start_date": start_date,  # "1999-01-01",
+                            "end_date": self.end_date,  # "2019-11-27",
+                            "bar_size": "1 day",
+                            "price_type": "TRADES"
+                        }
+                        arranged_params = HistoricalRequestTemplate(params).params
+                        contract['exchange'] = 'SMART'
+                        status = self.ib_client.request_historical_data(contract, arranged_params)
+                        self.request_scheduler.request_added()
 
-                self.msg_queue.task_done()
-                logger.info('Historical data request sent : {} {} {}'\
-                            .format(contract['symbol'], start_date, self.end_date))
+                        self.msg_queue.task_done()
+                        logger.info('Historical data request sent : {} {} {}'\
+                                    .format(contract['symbol'], start_date, self.end_date))
+                    except:
+                        logger.exception('exception while sending historical request'.format(contract['symbol']))
         except:
             logger.exception('unhandled exception in HistoricalRunner for {}'.format(contract['symbol']))
