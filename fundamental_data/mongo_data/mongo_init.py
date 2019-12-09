8756e443aef896adfdabbc501934396adc673ef0
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-12-09 19:52:32 +0000

    bug in historical data end

--- fundamental_data/mongo_data/mongo_init.py
+++ fundamental_data/mongo_data/mongo_init.py
@@ -5,9 +5,9 @@ import mongoengine
 
 def global_init(config: ConfigParser):
     mongoengine.register_connection(alias='financialDataRepository',
-                                    name=config.get('db'),
-                                    host=config.get('host'),
-                                    port=config.getint('port')
+                                    name=config.get('mongo', 'db'),
+                                    host=config.get('mongo', 'host'),
+                                    port=config.getint('mongo', 'port')
                                     )
     # username='ib_test',
     # password='ib_test')
