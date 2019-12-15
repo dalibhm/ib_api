f14f8e96fa83e154096b29a1f3f1b01a699ffee7
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2019-12-15 17:25:20 +0000

    setting the environment and running the app

--- deploy/set_environment.sh
+++ deploy/set_environment.sh
@@ -10,17 +10,9 @@ BASE_DIR=.
 #git clone https://github.com/dali1981/ib_api.git
 
 #create environment for services
-for service in ib_client fundamental_data stock_listing runner
+for service in ib_client fundamental_data historical_data stock_listing runner
 do
   python3 -m venv ${BASE_DIR}/${service}/venv
   ${BASE_DIR}/${service}/venv/bin/pip install --upgrade pip setuptools
   ${BASE_DIR}/${service}/venv/bin/pip install -r ${BASE_DIR}/${service}/requirements.txt
 done
-
-
-./historical_data/venv/bin/python ./historical_data/historical_data_server.py & disown
-./venv/bin/python ./historical_data_server.py & disown
-./ib_client/venv/bin/python ./ib_client/app.py & disown
-./venv/bin/python app.py & disown
-./runner/venv/bin/python ./runner/runner.py --historical --start-date 1999-01-01 --end-date 2019-11-27 --stock-number 10000 & disown
-./venv/bin/python ./runner.py --historical --start-date 1999-01-01 --end-date 2019-11-27 --stock-number 40000 & disown
--- run.services.sh
+++ run.services.sh
@@ -1,9 +1,7 @@
-./data_api/venv/bin/python ./data_api/flask_app.py runserver
-./ib_client/venv/bin/python ./ib_client/app.py
-
-
-./venv/bin/python ./historical_data_server.py
-venv/bin/python fundamental_server.py
-
-historical_data/venv/bin/python historical_data/historical_data_server.py
+./historical_data/venv/bin/python ./historical_data/historical_data_server.py & disown
+./venv/bin/python ./historical_data_server.py & disown
+./ib_client/venv/bin/python ./ib_client/app.py & disown
+./venv/bin/python app.py & disown
+#./runner/venv/bin/python ./runner/runner.py --historical --start-date 1999-01-01 --end-date 2019-11-27 --stock-number 10000 & disown
+#./venv/bin/python ./runner.py --historical --start-date 1999-01-01 --end-date 2019-11-27 --stock-number 40000 & disown
 
