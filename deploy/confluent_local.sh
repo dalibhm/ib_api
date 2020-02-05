54aa0b1225c2b91dafb7dbb73f21ef90b6247eb8
Mohamed-Ali Ben Hamouda <mohamedali.benhamouda@gmail.com>
2020-02-05 08:35:27 +0000

    added intgration

--- deploy/confluent_local.sh
+++ deploy/confluent_local.sh
@@ -1,13 +1,26 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+sudo apt-get install openjdk-8-jdk
+
+install java before !!!
+make sure there is only one java
+
+
 sudo apt install curl
 curl -O http://packages.confluent.io/archive/5.3/confluent-5.3.1-2.12.tar.gz
 
 tar xzf confluent-5.3.1-2.12.tar.gz
+tar -xvf confluent-5.3.1-2.12.tar
 
 # /usr/local/bin was empty in the new machine
 # needs to be run as root
 curl -L https://cnfl.io/cli | sh -s -- -b /usr/local/bin
 
 export PATH=<path-to-confluent>/bin:$PATH
+# /usr/local/bin is cormally in the path
+
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+
 
 confluent local start
 
--- deploy/make-venv.sh
+++ deploy/make-venv.sh
@@ -8,7 +8,7 @@ BASE_DIR=..
 #git clone https://github.com/dali1981/ib_api.git
 
 #create environment for services
-apt ger python3-venv python3-pip
+apt install python3-venv python3-pip
 
 for service in runner ib_client stock_listing contracts historical_data fundamental_data
 do
@@ -16,5 +16,7 @@ do
   ${BASE_DIR}/${service}/venv/bin/pip install --upgrade pip setuptools
   ${BASE_DIR}/${service}/venv/bin/pip install -r ${BASE_DIR}/${service}/requirements.txt
 done
+cd ${BASE_DIR}/resources
+../ib_client/venv/bin/python3 setup.py install
 
-${BASE_DIR}/ib_client/venv/bin/python3 ${BASE_DIR}/resources/setup.py install
+venv/bin/pip3 install -i https://testpypi.python.org/pypi psycopg2==2.7.7
